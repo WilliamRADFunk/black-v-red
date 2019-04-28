a99cd8c2bba08d6c86cb2f6387826be4c1211e8c
William RAD Funk <contact@WILLIAMRADFunk.com>
2019-04-28 12:54:16 -0400

    Starts fleshing out types.

---
+++ src/ts/models/critter.ts
@@ -0,0 +1,9 @@
+export interface Critter {
+    aggressive: boolean;
+    armor: number;
+    armorType: string;
+    damagePerTurn: number;
+    health: number;
+    speed: number;
+    speedModifier: number;
+}
